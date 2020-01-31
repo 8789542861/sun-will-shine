1952fddf7bbca4ad8f5ac7aa473cb87c8260c47c
8789542861 <53976860+8789542861@users.noreply.github.com>
2020-01-31 22:16:55 +0530

    Add files via upload

---
+++ ty.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+
+ 
+public class JavaApplication1 {
+
+     
+    public static void main(String[] args) {
+       
+        Scanner fufa = new Scanner(System.in);
+        System.out.print("enter the value of a  :");
+        int a=fufa.nextInt();
+        System.out.print("enter the value of b  :");
+        int b=fufa.nextInt();
+        int f=a+b;
+       
+        System.out.print("the summation of a+b is :"  +f);
+        
+      
+        // TODO code application logic here
+    }
+    
+}
